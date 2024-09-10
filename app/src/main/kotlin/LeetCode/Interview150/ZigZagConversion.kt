ca8efcefff992768a7271a629687b566dc6ad4e3
Lee Chilvers <lee.chilvers@icloud.com>
2024-09-10 01:52:46 +0100

    Better Zig Zag Conversion solution

--- app/src/main/kotlin/LeetCode/Interview150/ZigZagConversion.kt
+++ app/src/main/kotlin/LeetCode/Interview150/ZigZagConversion.kt
@@ -2,31 +2,20 @@
 package LeetCode
 
 final class ZigZagConversion {
-    // add to 2D array and read row-by-row
     fun solution(s: String, numRows: Int): String {
         if (numRows == 1) return s
-        val matrix: Array<Array<Char?>> = Array(numRows) { 
-            Array(s.length) { null }
-        }
         
-        // build matrix
-        var zig = false
-        var i = 0
-        var j = 0
-        for (c in s) {
-            matrix[i][j] = c
-            if (i == matrix.lastIndex || i == 0) zig = !zig // toggle down <-> up-across
-            i = if (zig) i+1 else i-1
-            if (!zig) j++
-        }
+        var sb = StringBuilder()
+        var index = MutableList(numRows) { 0 }
+        var ptr = 0
+        var dir = 1
 
-        // build string from matrix
-        val sb = StringBuilder()
-        for (y in matrix.indices) {
-            for (x in matrix[y].indices) {
-                if (matrix[y][x] == null) continue
-                sb.append(matrix[y][x])
-            }
+        for (c in s) {
+            sb.insert(index[ptr], c)
+            for (i in ptr until index.size) index[i]++
+
+            ptr += dir
+            if (ptr == 0 || ptr == numRows-1) dir *= -1
         }
 
         return sb.toString()
